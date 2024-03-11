ecd77fdb7be9b72d81b574ae6445f0b233a22cf0
Anna manoelly <anna.mannuuh73@gmail.com>
2024-03-10 23:01:27 -0300

    feed pronto

---
+++ script.js
@@ -0,0 +1,29 @@
+var crl = document.getElementById('myCrl').getContext('2d');
+
+crl.beginPath();
+crl.arc(60, 100, 40, 0, 2 * Math.PI);
+crl.fillStyle = '#00BFFF';
+crl.fill();
+crl.linewidth = 5;
+crl.strokeStyle = '#dbdbdb';
+crl.stroke();
+
+var crl1 = document.getElementById('myCrl1').getContext('2d');
+
+crl1.beginPath();
+crl1.arc(60, 100, 40, 0, 2 * Math.PI);
+crl1.fillStyle = '#00BFFF';
+crl1.fill();
+crl1.linewidth = 5;
+crl1.strokeStyle = '#dbdbdb';
+crl1.stroke();
+
+var crl2 = document.getElementById('myCrl2').getContext('2d');
+
+crl2.beginPath();
+crl2.arc(60, 100, 40, 0, 2 * Math.PI);
+crl2.fillStyle = '#00BFFF';
+crl2.fill();
+crl2.linewidth = 5;
+crl2.strokeStyle = '#dbdbdb';
+crl2.stroke();
